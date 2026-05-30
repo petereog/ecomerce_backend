bcf768df9dab9f66b9f2061c07b958f5d2f987bf
petereog <ogungbepeter1@gmail.com>
2026-05-30 18:17:45 +0100

    fix resend initialization

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,8 +1,8 @@
 const { Resend } = require('resend');
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const sendOTP = async (email, otp) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  
   await resend.emails.send({
     from: 'Pecumat Foods <onboarding@resend.dev>',
     to: email,
