da9321a48f60a78d49f89d52fe6daaac5b26c2e9
petereog <ogungbepeter1@gmail.com>
2026-05-30 18:28:27 +0100

    fix dotenv for production

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const PORT = process.env.PORT || 5000;
 
