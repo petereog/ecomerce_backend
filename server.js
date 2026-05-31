29ad4f039aa661bc7389fc84149e81810db53066
petereog <ogungbepeter1@gmail.com>
2026-05-31 05:11:26 +0100

    remove dotenv let railway handle env

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
